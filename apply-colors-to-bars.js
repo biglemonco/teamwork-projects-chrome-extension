a2d3f73ed679fd7a2d6c5ade0e916f521fe19e6d
Owen Richards <owen@biglemon.co.uk>
2020-05-29 15:27:42 +0100

    Initial working submission to the Chrome store

---
+++ apply-colors-to-bars.js
@@ -0,0 +1,63 @@
+
+/**
+ * Create an object mapping project IDs to colors
+ */
+const createProjectIdColorMap = function() {
+	const $labels = $('.vis-panel.vis-left > .vis-content > .vis-labelset > .vis-label');
+	const map = {};
+	$labels.each(function (index, $label) {
+		const id = $($label).find('span.tipped-delegate:first').data('id');
+		const color = $($label).find('.project-info:first').css('border-color');
+		map[id] = color;
+	})
+	return map;
+}
+
+/**
+ * Apply the color map to the matching bars in the chart
+ * @param {object} colorMap 
+ */
+const applyColorsToChartBars = function(colorMap) {
+	Object.keys(colorMap).forEach(function (id) {
+		const color = colorMap[id];
+		if (color.replace(/\s/g, "") === 'rgba(0,0,0,0)') return;
+		const $el = $('.vis-tw-project#project-' + id);
+		$el.css('border-color', color)
+		$el.closest('.vis-item').css('border-color', color)
+		$el.css('background', color)
+		$el.closest('.vis-item').css('background', color)
+		if ($el.hasClass('vis-tw-project-upcoming')) {
+			$el.closest('.vis-item').css('opacity', 0.6)
+		} else if ($el.hasClass('vis-tw-project-completed')) {
+			$el.closest('.vis-item').css('opacity', 0.3)
+		}
+	})
+}
+
+var done = false;
+var attempts = 0;
+function checkForElements() {
+
+	attempts++;
+	checkStopInterval();
+
+	if ($('.vis-timeline .vis-item.vis-tw-project').length < 2) {
+		return;
+	}
+
+	const colorMap = createProjectIdColorMap();
+	// console.log(colorMap)
+
+	if (Object.keys(colorMap).length > 0) {
+		done = true;
+		applyColorsToChartBars(colorMap);
+	}
+}
+
+function checkStopInterval() {
+	if (attempts > 15 || done) {
+		clearInterval(checkInterval);
+	}
+}
+
+const checkInterval = setInterval(checkForElements, 500);
